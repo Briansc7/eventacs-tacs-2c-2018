e89fefb8c46c0bd15564884ffffa73fe842d8ad5
facufigo <facufigo@hotmail.com>
2018-08-25 00:58:33 -0300

    fixes in server and organizing pom

--- src/main/java/com/eventacs/controllers/HealthCheckController.java
+++ src/main/java/com/eventacs/controllers/HealthCheckController.java
@@ -3,12 +3,14 @@ package com.eventacs.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/eventacs")
 public class HealthCheckController {
 
     @RequestMapping(value = "/health-check" , method = RequestMethod.GET)
+    @ResponseBody
     public String getHealthState () {
         return "Status: Ok, App online.";
     }
