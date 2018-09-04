9b649aa4a15af4ec27a2ea089aaa2c9789c363d3
sebastianchaves <sebastian.chaves@despegar.com>
2018-09-04 11:47:43 -0300

    Logback config xml

--- src/main/java/com/eventacs/HealthCheckController.java
+++ src/main/java/com/eventacs/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.eventacs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/eventacs")
 public class HealthCheckController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
+
     @RequestMapping(value = "/health-check" , method = RequestMethod.GET)
     @ResponseBody
     public String getHealthState() {
+        LOGGER.info("/eventacs/health-check [GET]");
         return "Status: Ok, App online.";
     }
 
