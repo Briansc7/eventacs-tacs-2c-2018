ea4f008ef0cf54556addf7b41a7b749635dade72
sebastianchaves <sebastian.chaves@despegar.com>
2018-10-07 19:24:00 -0300

    Fix files routes

--- src/main/java/com/eventacs/server/AppConfig.java
+++ src/main/java/com/eventacs/server/AppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.eventacs")
 public class AppConfig {
