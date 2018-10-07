b6ddeaa942be9cad2561be1159e47ab9be2143c1
facufigo <facufigo@hotmail.com>
2018-10-07 16:46:44 -0300

    adding js

---
+++ src/main/resources/static/js/validaciones.js
@@ -0,0 +1,7 @@
+var validaciones = (function () {
+    const regexFecha = new RegExp("^([0-9]{4})[-](0[1-9]|1[0-2])[-](0[1-9]|(1|2)[0-9]|3[0-1])$");
+    const url = "http://localhost:8080/search";
+
+
+
+})()
