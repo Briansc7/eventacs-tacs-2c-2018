dc4db0a96a26ea0ea3e03ac49a919c00be2e993e
facufigo <facufigo@hotmail.com>
2018-10-07 22:09:31 -0300

    finished url for events in front

--- src/main/resources/static/js/validaciones.js
+++ src/main/resources/static/js/validaciones.js
@@ -1,5 +1,6 @@
 var validaciones = (function () {
     const dateRegex = new RegExp("^([0-9]{4})[-](0[1-9]|1[0-2])[-](0[1-9]|(1|2)[0-9]|3[0-1])$");
+    const regexInput = new RegExp("([0-9]{1})[,]|[0-9]{1}");
     const url = "http://localhost:8080/events";
 
     var keyWord;
